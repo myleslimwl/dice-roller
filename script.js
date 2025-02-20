23d771b63d1e59015a6094c681c045c6cb58a87f
myleslim <mousee1030@gmail.com>
2025-02-20 15:19:32 +0800

    more javascript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function rollDice() {
+
   const diceCount = document.getElementById("diceCount").value;
   const diceResult = document.getElementById("diceResult");
   const diceImage = document.getElementById("diceImage");
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < diceCount; i++) {
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    values.push(randomValue);
+    images.push(`images/dice${randomValue}.png`);
+  }
+
+  diceResult.textContent = `dice: ${values.join(", ")}`;
+  diceImage.innerHTML = values.join("");
 }
