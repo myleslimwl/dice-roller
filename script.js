ce935cf6c09dffbb92e129e9f033dc61e914b2e9
myleslim <mousee1030@gmail.com>
2025-02-20 14:15:44 +0800

    declared initial variables

---
+++ script.js
@@ -0,0 +1,5 @@
+function rollDice() {
+  const diceCount = document.getElementById("diceCount").value;
+  const diceResult = document.getElementById("diceResult");
+  const diceImage = document.getElementById("diceImage");
+}
